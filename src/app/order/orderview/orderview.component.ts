72f4397ed7b52f2cec9379ff34ce9ef8b8551445
marcoantonio0 <53385424+marcoantonio0@users.noreply.github.com>
2020-10-18 14:13:19 -0300

    Marco
    orderview update

--- src/app/order/orderview/orderview.component.ts
+++ src/app/order/orderview/orderview.component.ts
@@ -9,15 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderviewComponent implements OnInit {
   public order;
+  public status = [
+    {
+      description: 'Aguardando orçamento',
+      idstatus: 1,
+      active: false
+    },
+    {
+      description: 'Aguardando pagamento',
+      idstatus: 2,
+      active: false
+    },
+    {
+      description: 'Pagamento em analise',
+      idstatus: 3,
+      active: false
+    },
+    {
+      description: 'Projeto em execução',
+      idstatus: 4,
+      active: false
+    },
+    {
+      description: 'Projeto em análise',
+      idstatus: 5,
+      active: false
+    },
+    {
+      description: 'Concluído',
+      idstatus: 6,
+      active: false
+    }
+  ];
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private sOrder: OrderService
   ) {
     this.route.params.subscribe(params => {
-      if(params) {
+      if(Object.keys(params).length > 0) {
           this.sOrder.getOrderById(params.id).subscribe(r => {
             this.order = r;
+            this.setStatus(r.status);
           }, e => {
             this.router.navigate(['/order']);
           });
@@ -28,6 +61,30 @@ export class OrderviewComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  setStatus(array){
+    for (const status of array) {
+      const index = this.status.findIndex(x => x.idstatus === status.idstatus);
+      if(index > -1){
+        status['active'] = true;
+        this.status[index] = status;
+      }
+    }
+    console.log(this.status);
+  }
+
+  getSizeTimeline(){
+    const stSize = [];
+    for (const st of this.status) {
+      if(st.active === true){
+        stSize.push(st);
+      }
+    }
+    if(stSize.length === 6){
+      return 100;
+    }
+    return Math.floor(((stSize.length * 100) / this.status.length - 5));
   }
 
 }
